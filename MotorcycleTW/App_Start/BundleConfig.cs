09c64e6705d78886c9f9e4fb61324f0fce2d223f
abcd1 <ma26951197@gmail.com>
2019-07-15 17:22:53 +0800

    OK

--- MotorcycleTW/App_Start/BundleConfig.cs
+++ MotorcycleTW/App_Start/BundleConfig.cs
@@ -25,6 +25,8 @@ namespace MotorcycleTW
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/chart.js").Include(
+                      "~/Scripts/Chart.min.js"));
         }
     }
 }
---
+++ MotorcycleTW/Controllers/Dashboard/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MotorcycleTW.Controllers.Dashboard
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult DashboardDesiner()
+        {
+            return View("DashboardDesiner");
+        }
+    }
+}
