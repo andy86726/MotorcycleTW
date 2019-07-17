f623b99c8454d8c20bc4d2e6400665cb30591012
abcd1 <ma26951197@gmail.com>
2019-07-17 16:37:39 +0800

    ok

---
+++ MotorcycleTW/Models/MemberInfoamtion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MotorcycleTW.Models
+{
+    public class MemberInfoamtion
+    {
+        [StringLength(10, ErrorMessage = "{0} 的長度必須為 {2} 個字元。", MinimumLength = 10)]
+        [Display(Name = "身分證字號/統一編號")]
+        public string Identit { get; set; }
+    }
+}
