43012f511a718ad104f876aab041cb6767776129
曾子昂 <andy86726@gmail.com>
2019-07-25 12:11:55 +0800

    ok

--- MotorcycleTW/Models/MotorclcleDB_1.cs
+++ MotorcycleTW/Models/MotorclcleDB_1.cs
@@ -5,10 +5,10 @@ namespace MotorcycleTW.Models
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Linq;
 
-    public partial class MrtorcycleContext : DbContext
+    public partial class MotorclcleDB_1 : DbContext
     {
-        public MrtorcycleContext()
-            : base("name=MrtorcycleDB")
+        public MotorclcleDB_1()
+            : base("name=MotorclcleDB_1")
         {
         }
 
@@ -27,6 +27,7 @@ namespace MotorcycleTW.Models
         public virtual DbSet<Shopping_Cart> Shopping_Cart { get; set; }
         public virtual DbSet<Stores> Stores { get; set; }
         public virtual DbSet<sysdiagrams> sysdiagrams { get; set; }
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Products>()
@@ -38,6 +39,5 @@ namespace MotorcycleTW.Models
                 .WithOptional(e => e.Stores)
                 .HasForeignKey(e => e.Delive_Way_s_id);
         }
-
     }
 }
