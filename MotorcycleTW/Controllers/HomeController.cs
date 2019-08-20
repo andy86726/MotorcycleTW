406c42a163c090a50153dc5266ad6fff2d74052c
曾子昂 <andy86726@gmail.com>
2019-08-20 11:44:43 +0800

    加入solopage

--- MotorcycleTW/Controllers/HomeController.cs
+++ MotorcycleTW/Controllers/HomeController.cs
@@ -165,5 +165,9 @@ namespace MotorcycleTW.Controllers
         {
             return View();
         }
+        public ActionResult solopagee()
+        {
+            return View();
+        }
     }
 }
